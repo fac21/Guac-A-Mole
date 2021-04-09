a4c2eee6ace82c3d4e1634760ba55979dd77cb6f
Safia <safiaali@hotmail.com>
2021-04-09 14:42:20 +0100

    add randomTime and randomBowl function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // get html elements from DOM
 const bowls = document.querySelectorAll('.bowls');
-const avacados = document.querySelectorAll('.avacado');
+const avocados = document.querySelectorAll('.avocado');
 const scoreBoard = document.querySelectorAll('.score');
+let lastBowl;
 
+//avocados pop up for a random time
+function randomTime(min, max){
+    return Math.round(Math.random()* (max - min) + min);
+}
+
+// avocadoes pop out of a random bowl (1 to 6)
+function randomBowl (bowls){
+    const index = Math.floor(Math.random()* bowls.length);
+    const bowl = bowls[index];
+    if (bowl === lastBowl){
+        return randomBowl(bowls);
+    }
+    lastBowl = bowl;
+    return bowl;
+}
