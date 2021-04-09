02619b75811bcc73954a9a965fbc44eb9489e42a
Safia <safiaali@hotmail.com>
2021-04-09 11:53:40 +0100

    Add html skelton Relates #1

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// get html elements from DOM
+const bowls = document.querySelectorAll('.bowls');
+const avacados = document.querySelectorAll('.avacado');
+const scoreBoard = document.querySelectorAll('.score');
 
